7cc9b7ac3e4ff59a2b41a9a7d41e70fcbc20a7a1
Ivan Kyrylov <81202331+Kivanval@users.noreply.github.com>
2023-03-31 16:59:10 +0300

    Spring Setup (#1)

---
+++ src/main/java/com/qunite/api/QuniteApiApplication.java
@@ -0,0 +1,13 @@
+package com.qunite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuniteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuniteApiApplication.class, args);
+    }
+
+}
