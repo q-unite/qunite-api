05422d4f16cad50dc522781d03e1b15cb26f1dba
knurff <beryndaviktor@gmail.com>
2023-04-01 01:51:34 +0300

    Test

--- src/main/java/com/qunite/api/QuniteApiApplication.java
+++ src/main/java/com/qunite/api/QuniteApiApplication.java
@@ -1,7 +1,8 @@
 package com.qunite.api;
 
-import java.time.Instant;
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
