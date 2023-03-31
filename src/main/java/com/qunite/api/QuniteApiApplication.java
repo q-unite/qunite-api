2883e72030f96654c6b25cb94307cf43c1320fed
knurff <beryndaviktor@gmail.com>
2023-04-01 02:37:43 +0300

    Test

--- src/main/java/com/qunite/api/QuniteApiApplication.java
+++ src/main/java/com/qunite/api/QuniteApiApplication.java
@@ -1,24 +1,24 @@
 package com.qunite.api;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuniteApiApplication {
 
     public static void main(String[] args) {
-
-
-
-
         SpringApplication.run(QuniteApiApplication.class, args);
-
-
-
-
-
     }
 
+
+    private void DoNothing(){
+
+    }
 }
