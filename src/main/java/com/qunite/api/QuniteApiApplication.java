b12e81ec2e7de152dabff9d11bd585d63609f8eb
knurff <beryndaviktor@gmail.com>
2023-04-05 00:09:16 +0300

    Clean up

--- src/main/java/com/qunite/api/QuniteApiApplication.java
+++ src/main/java/com/qunite/api/QuniteApiApplication.java
@@ -1,13 +1,24 @@
 package com.qunite.api;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuniteApiApplication {
 
     public static void main(String[] args) {
+
+
+
+
         SpringApplication.run(QuniteApiApplication.class, args);
+
+
+
+
+
     }
 
 }
