37a0f961292b19faad21ef324075509ef86459aa
knurff <beryndaviktor@gmail.com>
2023-04-05 00:09:34 +0300

    Revert "Test"
    This reverts commit 05422d4f16cad50dc522781d03e1b15cb26f1dba.

--- src/main/java/com/qunite/api/QuniteApiApplication.java
+++ src/main/java/com/qunite/api/QuniteApiApplication.java
@@ -1,8 +1,7 @@
 package com.qunite.api;
 
+import java.time.Instant;
 import org.springframework.boot.SpringApplication;
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
