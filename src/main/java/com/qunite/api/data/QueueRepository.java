c05c7769fe46d829d997e41bf3f9894bdf5645d9
kivanval <kivanval@gmail.com>
2023-04-07 04:49:48 +0300

    Add config files and refactor code

--- src/main/java/com/qunite/api/data/QueueRepository.java
+++ src/main/java/com/qunite/api/data/QueueRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QueueRepository extends JpaRepository<Queue, Long> {
-    boolean existsByCreatorId(Long id);
+  boolean existsByCreatorId(Long id);
 }
--- src/main/java/com/qunite/api/domain/Queue.java
+++ src/main/java/com/qunite/api/domain/Queue.java
@@ -15,17 +15,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.Hibernate;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 @Entity
 @Table(name = "QUEUES")
@@ -33,82 +32,86 @@ import java.util.Objects;
 @NoArgsConstructor
 public class Queue {
 
-    @Id
-    @Getter
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+  @Id
+  @Getter
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
 
-    @Getter
-    @Setter
-    @Column
-    String name;
+  @Getter
+  @Setter
+  @Column
+  String name;
 
-    @Getter
-    @Setter
-    @ToString.Exclude
-    @ManyToOne
-    @JoinColumn(name = "creator_id")
-    User creator;
+  @Getter
+  @Setter
+  @ToString.Exclude
+  @ManyToOne
+  @JoinColumn(name = "creator_id")
+  User creator;
 
-    @Access(AccessType.FIELD)
-    @ToString.Exclude
-    @OrderBy("id asc")
-    @ManyToMany(cascade = CascadeType.REMOVE)
-    @JoinTable(name = "QUEUES_MANAGERS",
-            joinColumns = @JoinColumn(name = "queue_id"),
-            inverseJoinColumns = @JoinColumn(name = "manager_id"))
-    List<User> managers = new ArrayList<>();
+  @Access(AccessType.FIELD)
+  @ToString.Exclude
+  @OrderBy("id asc")
+  @ManyToMany(cascade = CascadeType.REMOVE)
+  @JoinTable(name = "QUEUES_MANAGERS",
+      joinColumns = @JoinColumn(name = "queue_id"),
+      inverseJoinColumns = @JoinColumn(name = "manager_id"))
+  List<User> managers = new ArrayList<>();
 
-    @Access(AccessType.FIELD)
-    @OrderBy("createdAt asc, id asc")
-    @ToString.Exclude
-    @OneToMany(mappedBy = "queue", cascade = CascadeType.REMOVE)
-    List<Entry> entries = new ArrayList<>();
+  @Access(AccessType.FIELD)
+  @OrderBy("createdAt asc, id asc")
+  @ToString.Exclude
+  @OneToMany(mappedBy = "queue", cascade = CascadeType.REMOVE)
+  List<Entry> entries = new ArrayList<>();
 
-    @Getter
-    @Column(name = "created_at")
-    Instant createdAt = Instant.now();
+  @Getter
+  @Column(name = "created_at")
+  Instant createdAt = Instant.now();
 
-    public void addManager(User manager) {
-        managers.add(manager);
-        manager.managedQueues.add(this);
+  public void addManager(User manager) {
+    managers.add(manager);
+    manager.managedQueues.add(this);
+  }
+
+  public void removeManager(User manager) {
+    managers.remove(manager);
+    manager.managedQueues.remove(this);
+  }
+
+  public void addEntry(Entry entry) {
+    entries.add(entry);
+    entry.setQueue(this);
+  }
+
+  public void removeEntry(Entry entry) {
+    entries.remove(entry);
+    entry.member.entries.remove(entry);
+    entry.setMember(null);
+    entry.setQueue(null);
+  }
+
+  public List<Entry> getEntries() {
+    return Collections.unmodifiableList(entries);
+  }
+
+  public List<User> getManagers() {
+    return Collections.unmodifiableList(managers);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public void removeManager(User manager) {
-        managers.remove(manager);
-        manager.managedQueues.remove(this);
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
     }
+    Queue queue = (Queue) o;
+    return getId() != null && Objects.equals(getId(), queue.getId());
+  }
 
-    public void addEntry(Entry entry) {
-        entries.add(entry);
-        entry.setQueue(this);
-    }
-
-    public void removeEntry(Entry entry) {
-        entries.remove(entry);
-        entry.member.entries.remove(entry);
-        entry.setMember(null);
-        entry.setQueue(null);
-    }
-
-    public List<Entry> getEntries() {
-        return Collections.unmodifiableList(entries);
-    }
-
-    public List<User> getManagers() {
-        return Collections.unmodifiableList(managers);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Queue queue = (Queue) o;
-        return getId() != null && Objects.equals(getId(), queue.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
