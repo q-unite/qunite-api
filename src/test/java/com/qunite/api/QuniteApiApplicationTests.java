fa05291b16288548bc46814fa18a28caa1ee7d26
knurff <91338352+knurff@users.noreply.github.com>
2023-04-09 03:52:21 +0300

    Add testcontainers support (#21)

--- src/test/java/com/qunite/api/QuniteApiApplicationTests.java
+++ src/test/java/com/qunite/api/QuniteApiApplicationTests.java
@@ -1,9 +1,7 @@
 package com.qunite.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class QuniteApiApplicationTests {
 
   @Test
