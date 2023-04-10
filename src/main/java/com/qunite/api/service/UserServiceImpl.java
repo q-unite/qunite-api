2053e7ccca881d1d5ad17f4c2e814742c2723ca7
booravch1que <kozerog07012004@gmail.com>
2023-04-11 02:37:08 +0300

    checkstyle fix

--- src/main/java/com/qunite/api/service/UserServiceImpl.java
+++ src/main/java/com/qunite/api/service/UserServiceImpl.java
@@ -2,30 +2,29 @@ package com.qunite.api.service;
 
 import com.qunite.api.data.UserRepository;
 import com.qunite.api.domain.User;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-    @Override
-    public void createUser(User user) {
-        userRepository.save(user);
-    }
+  @Override
+  public void createUser(User user) {
+    userRepository.save(user);
+  }
 
-    @Override
-    public Optional<User> getUser(Long id) {
-        return Optional.ofNullable(id).flatMap(userRepository::findById);
-    }
+  @Override
+  public Optional<User> getUser(Long id) {
+    return Optional.ofNullable(id).flatMap(userRepository::findById);
+  }
 
-    @Override
-    public void deleteUser(Long id) {
-        Optional.ofNullable(id).ifPresent(userRepository::deleteById);
-    }
+  @Override
+  public void deleteUser(Long id) {
+    Optional.ofNullable(id).ifPresent(userRepository::deleteById);
+  }
 
 }
