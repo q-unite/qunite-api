afc7ae56cf48d7b348b1b99326fb16fe74835250
booravch1que <89353568+booravch1que@users.noreply.github.com>
2023-04-12 00:53:16 +0300

    Update users-create.sql

--- src/test/resources/users-create.sql
+++ src/test/resources/users-create.sql
@@ -1,8 +1,7 @@
-INSERT INTO users (id, first_name, last_name)
-VALUES (1, 'First', 'User'),
-       (2, 'Second', 'User'),
-       (3, 'Third', 'User'),
-       (4, 'Fourth', 'User'),
-       (5, 'Fifth', 'User'),
-       (6, 'Sixth', 'User'),
-       (7, 'Seventh', 'User');
+INSERT INTO users VALUES (1, 'First', 'User'),
+                         (2, 'Second', 'User'),
+                         (3, 'Third', 'User'),
+                         (4, 'Fourth', 'User'),
+                         (5, 'Fifth', 'User'),
+                         (6, 'Sixth', 'User'),
+                         (7, 'Seventh', 'User');
