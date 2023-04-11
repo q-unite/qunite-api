af4fa629d827de19a9eb4003ab35bef3c672ef37
booravch1que <89353568+booravch1que@users.noreply.github.com>
2023-04-12 00:52:46 +0300

    Delete manager-create.sql

--- src/test/resources/manager-create.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users
-VALUES (1, 'Creator', 'Creator');
-INSERT INTO users
-VALUES (2, 'Manager', 'Manager');
-
-INSERT INTO queues (id, name, creator_id, created_at)
-VALUES (1, 'First queue', 1, now());
-INSERT INTO QUEUES_MANAGERS (queue_id, manager_id)
-VALUES (1, 2)
