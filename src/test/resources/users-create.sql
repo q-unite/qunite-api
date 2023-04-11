0dc1df71b6bdad84ea9c802e66141453762d27ed
booravch1que <kozerog07012004@gmail.com>
2023-04-12 00:49:58 +0300

    fix pr 3

--- src/test/resources/users-create.sql
+++ src/test/resources/users-create.sql
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 INSERT INTO users
+=======
+INSERT INTO users (id, first_name, last_name)
+>>>>>>> c369eb6621f55d62e678c284f2d7f3bef474455e
 VALUES (1, 'First', 'User'),
        (2, 'Second', 'User'),
        (3, 'Third', 'User'),
