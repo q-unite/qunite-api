bf8f1a9a3f3419acb99dfbf4904c803fadafde7f
NikPaukov <sirelfarionik@gmail.com>
2023-06-14 23:12:06 +0300

    fix/jsonView

--- src/main/java/com/qunite/api/web/dto/QueueDto.java
+++ src/main/java/com/qunite/api/web/dto/QueueDto.java
@@ -3,15 +3,19 @@ package com.qunite.api.web.dto;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * A DTO for the {@link com.qunite.api.domain.Queue} entity
  */
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class QueueDto implements Serializable {
-  private final Long id;
-  private final String name;
-  private final Long creatorId;
-  private final String createdAt;
+  private Long id;
+  private String name;
+  private Long creatorId;
+  private String createdAt;
 }
--- src/main/java/com/qunite/api/web/dto/UserDto.java
+++ src/main/java/com/qunite/api/web/dto/UserDto.java
@@ -4,17 +4,21 @@ import com.fasterxml.jackson.annotation.JsonView;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * A DTO for the {@link com.qunite.api.domain.User} entity
  */
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class UserDto implements Serializable {
-  private final Long id;
+  private Long id;
   @JsonView(Views.Patch.class)
-  private final String firstName;
+  private String firstName;
   @JsonView(Views.Patch.class)
-  private final String lastName;
+  private String lastName;
 }
