a47c4215d8838075573987bf30338ad33637d4dc
xd6d <kurkin566@gmail.com>
2023-06-17 15:20:03 +0300

    deleted @Nullable

--- src/main/java/com/qunite/api/web/dto/user/UserUpdateDto.java
+++ src/main/java/com/qunite/api/web/dto/user/UserUpdateDto.java
@@ -1,12 +1,9 @@
 package com.qunite.api.web.dto.user;
 
 import lombok.Data;
-import org.springframework.lang.Nullable;
 
 @Data
 public class UserUpdateDto {
-  @Nullable
   private String firstName;
-  @Nullable
   private String lastName;
 }
