f15ac858f410e0621d802c6b1effac7661772e95
NikPaukov <sirelfarionik@gmail.com>
2023-07-07 16:30:31 +0300

    code-smell

--- src/test/java/com/qunite/api/web/AuthenticationIntegrationTest.java
+++ src/test/java/com/qunite/api/web/AuthenticationIntegrationTest.java
@@ -17,8 +17,6 @@ import com.qunite.api.web.dto.auth.AuthenticationRequest;
 import com.qunite.api.web.mapper.UserMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
