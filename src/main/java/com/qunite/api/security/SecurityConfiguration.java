619530dc89b30bb0de466f20dbbd209db1a35fb4
vetterocki <vitbutriy@gmail.com>
2023-07-07 16:12:04 +0300

    fixed Swagger 403 response bug

--- src/main/java/com/qunite/api/security/SecurityConfiguration.java
+++ src/main/java/com/qunite/api/security/SecurityConfiguration.java
@@ -25,7 +25,10 @@ public class SecurityConfiguration {
         .and()
         .authorizeHttpRequests(requests -> requests
             .requestMatchers("/auth/**").permitAll()
-            .requestMatchers("/swagger-ui.html").permitAll()
+            .requestMatchers("/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
