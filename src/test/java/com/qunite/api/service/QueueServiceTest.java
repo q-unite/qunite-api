2794d20445346721c0675903500e98fc3b88de0a
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-07-09 02:15:01 +0300

    Append queue service tests

--- src/test/java/com/qunite/api/service/QueueServiceTest.java
+++ src/test/java/com/qunite/api/service/QueueServiceTest.java
@@ -1,6 +1,7 @@
 package com.qunite.api.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,6 +12,10 @@ import com.qunite.api.data.QueueRepository;
 import com.qunite.api.data.UserRepository;
 import com.qunite.api.domain.EntryId;
 import com.qunite.api.domain.Queue;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,5 +86,76 @@ class QueueServiceTest {
     var memberPosition = queueService.getMemberPositionInQueue(3L, 1L);
 
     assertThat(memberPosition).hasValue(5);
+  }
+
+  @Sql({"/users-create.sql", "/queues-create.sql", "/entries-create.sql"})
+  @Test
+  void testChangeMemberPositionForward() {
+    var expectedEntryIdList = List.of(
+        new EntryId(6L, 1L),
+        new EntryId(5L, 1L),
+        new EntryId(7L, 1L),
+        new EntryId(4L, 1L),
+        new EntryId(3L, 1L));
+
+    queueService.changeMemberPositionInQueue(7L, 1L, 2);
+    var actualEntryIdList = entryRepository.findEntriesIdsByQueueId(1L);
+
+    assertEquals(expectedEntryIdList, actualEntryIdList);
+  }
+
+  @Sql({"/users-create.sql", "/queues-create.sql", "/entries-create.sql"})
+  @Test
+  void testChangeMemberPositionBackward() {
+    var expectedEntryIdList = List.of(
+        new EntryId(7L, 1L),
+        new EntryId(6L, 1L),
+        new EntryId(3L, 1L),
+        new EntryId(5L, 1L),
+        new EntryId(4L, 1L));
+
+    queueService.changeMemberPositionInQueue(3L, 1L, 2);
+    var actualEntryIdList = entryRepository.findEntriesIdsByQueueId(1L);
+
+    assertEquals(expectedEntryIdList, actualEntryIdList);
+  }
+
+  @Sql({"/users-create.sql", "/queues-create.sql", "/entries-create.sql"})
+  @Test
+  void testChangeMemberPositionOnItself() {
+    var expectedEntryIdList = List.of(
+        new EntryId(7L, 1L),
+        new EntryId(6L, 1L),
+        new EntryId(5L, 1L),
+        new EntryId(4L, 1L),
+        new EntryId(3L, 1L));
+
+    queueService.changeMemberPositionInQueue(5L, 1L, 2);
+    var actualEntryIdList = entryRepository.findEntriesIdsByQueueId(1L);
+
+    assertEquals(expectedEntryIdList, actualEntryIdList);
+  }
+
+  @Sql({"/users-create.sql", "/queues-create.sql", "/entries-create.sql"})
+  @Test
+  void testChangeMemberPositionConcurrent() throws InterruptedException {
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    executor.execute(() -> queueService.changeMemberPositionInQueue(7L, 1L, 4));
+    executor.execute(() -> queueService.changeMemberPositionInQueue(3L, 1L, 0));
+    executor.shutdown();
+    executor.awaitTermination(1, TimeUnit.MINUTES);
+    var actualEntryIdList = entryRepository.findEntriesIdsByQueueId(1L);
+    var expectedEntryIdList = List.of(
+        new EntryId(7L, 1L),
+        new EntryId(6L, 1L),
+        new EntryId(5L, 1L),
+        new EntryId(4L, 1L),
+        new EntryId(3L, 1L));
+
+    assertAll(
+        () -> assertEquals(expectedEntryIdList.size(), actualEntryIdList.size()),
+        () -> assertFalse(actualEntryIdList.contains(null)),
+        () -> assertTrue(actualEntryIdList.containsAll(expectedEntryIdList))
+    );
   }
 }
