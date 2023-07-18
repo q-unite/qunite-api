995b653f42d60dab715a9ba438e2638ba1e9398b
xd6d <kurkin566@gmail.com>
2023-07-18 19:16:12 +0300

    ResponseEntityControllerAdvice refactor

--- src/main/java/com/qunite/api/web/ResponseEntityControllerAdvice.java
+++ src/main/java/com/qunite/api/web/ResponseEntityControllerAdvice.java
@@ -9,34 +9,51 @@ import com.qunite.api.exception.UserNotFoundException;
 import com.qunite.api.web.dto.ExceptionResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ResponseEntityControllerAdvice {
   @ExceptionHandler({
-      QueueNotFoundException.class,
-      UserNotFoundException.class,
-      EntryNotFoundException.class})
-  public ResponseEntity<ExceptionResponse> handleNotFound(RuntimeException exception) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(exceptionResponse(exception.getMessage()));
-  }
-
-  @ExceptionHandler(UserAlreadyExistsException.class)
+      UserAlreadyExistsException.class,
+      IllegalArgumentException.class,
+      HttpMessageNotReadableException.class})
   public ResponseEntity<ExceptionResponse> handleBadRequest(RuntimeException exception) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(exceptionResponse(exception.getMessage()));
   }
 
-  @ExceptionHandler({
-      ForbiddenAccessException.class,
-      JWTDecodeException.class})
+  @ExceptionHandler(JWTDecodeException.class)
+  public ResponseEntity<ExceptionResponse> handleUnauthorized(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(exceptionResponse(exception.toString()));
+  }
+
+  @ExceptionHandler(ForbiddenAccessException.class)
   public ResponseEntity<ExceptionResponse> handleForbidden(RuntimeException exception) {
     return ResponseEntity.status(HttpStatus.FORBIDDEN)
         .body(exceptionResponse(exception.getMessage()));
+  }
+
+  @ExceptionHandler({
+      QueueNotFoundException.class,
+      UserNotFoundException.class,
+      EntryNotFoundException.class,
+      MethodArgumentTypeMismatchException.class})
+  public ResponseEntity<ExceptionResponse> handleNotFound(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(exceptionResponse(exception.getMessage()));
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ExceptionResponse> handleConflict(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(exceptionResponse(exception.toString()));
   }
 
   private ExceptionResponse exceptionResponse(String message) {
