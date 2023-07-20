9fb6839319e54ab683a52176550c454dd1365ee7
xd6d <kurkin566@gmail.com>
2023-07-20 19:29:20 +0300

    fix

--- src/main/java/com/qunite/api/web/ResponseEntityControllerAdvice.java
+++ src/main/java/com/qunite/api/web/ResponseEntityControllerAdvice.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
@@ -25,7 +24,6 @@ public class ResponseEntityControllerAdvice {
       IllegalArgumentException.class,
       HttpMessageNotReadableException.class,
       MethodArgumentTypeMismatchException.class})
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ExceptionResponse> handleBadRequest(RuntimeException exception) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(exceptionResponse(exception.getMessage()));
