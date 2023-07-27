98482d76182efac5cc6629130fc8fd9696b3fa41
Ashot Vdovychenko <ashotvdovichenko@gmail.com>
2023-07-27 05:06:33 +0300

    Append QueueController with leaving endpoint

--- src/main/java/com/qunite/api/web/controller/QueueController.java
+++ src/main/java/com/qunite/api/web/controller/QueueController.java
@@ -99,7 +99,18 @@ public class QueueController {
   public ResponseEntity<Void> deleteMember(@PathVariable Long id,
                                            @PathVariable(value = "member-id") Long memberId,
                                            Principal principal) {
-    queueService.deleteMember(memberId, id, principal.getName());
+    queueService.deleteMemberByCreatorOrManager(memberId, id, principal.getName());
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/{id}/members")
+  @Operation(summary = "Leave member from queue", responses = {
+      @ApiResponse(responseCode = "204"),
+      @ApiResponse(responseCode = "404", content = @Content())
+  })
+  public ResponseEntity<Void> leave(@PathVariable Long id,
+                                    Principal principal) {
+    queueService.leaveByMember(id, principal.getName());
     return ResponseEntity.noContent().build();
   }
 
