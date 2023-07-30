e8b57ea14a202695894307acc16f007a83334d49
NikPaukov <sirelfarionik@gmail.com>
2023-07-30 16:52:50 +0300

    checkstyle

--- src/main/java/com/qunite/api/data/UserRepository.java
+++ src/main/java/com/qunite/api/data/UserRepository.java
@@ -1,7 +1,6 @@
 package com.qunite.api.data;
 
 import com.qunite.api.domain.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
