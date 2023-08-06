83b9d22c8b02f1af5dec115fc1a12124ca3cbf35
xd6d <kurkin566@gmail.com>
2023-08-06 15:29:32 +0300

    Example values update due to DTO validation

--- src/main/java/com/qunite/api/web/dto/user/UserCreationDto.java
+++ src/main/java/com/qunite/api/web/dto/user/UserCreationDto.java
@@ -1,5 +1,6 @@
 package com.qunite.api.web.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,10 +13,12 @@ public class UserCreationDto {
   @NotBlank(message = "Specify username")
   @Pattern(regexp = "^\\w+$", message = "You can use a-z, 0-9 and underscores")
   @Size(min = 4, max = 32, message = "Enter at least 4 and less than 32 characters")
+  @Schema(example = "string")
   private String username;
 
   @NotBlank(message = "Specify email")
   @Email(message = "Enter correct email")
+  @Schema(example = "string@mail.com")
   private String email;
 
   @NotNull(message = "Specify password")
