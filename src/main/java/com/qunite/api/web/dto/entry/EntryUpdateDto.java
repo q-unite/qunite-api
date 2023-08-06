afdbba9ab4672eebd52e274d5892dcf1f68fa9a7
Artem Kurkin <kurkin566@gmail.com>
2023-08-06 12:56:37 +0300

    EntryUpdateDto fix validation (#112)

--- src/main/java/com/qunite/api/web/dto/entry/EntryUpdateDto.java
+++ src/main/java/com/qunite/api/web/dto/entry/EntryUpdateDto.java
@@ -1,10 +1,12 @@
 package com.qunite.api.web.dto.entry;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class EntryUpdateDto {
+  @NotNull(message = "Specify position")
   @PositiveOrZero(message = "Must not be negative")
   private Integer entryIndex;
 }
