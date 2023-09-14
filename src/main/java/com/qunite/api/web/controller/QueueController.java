4ff57c5ee9891892eb9cfe16ebbaa2781411798a
Artem Kurkin <kurkin566@gmail.com>
2023-09-14 14:17:03 +0300

    Add CORS (#119)
    * Added CORS support

--- src/main/java/com/qunite/api/web/controller/QueueController.java
+++ src/main/java/com/qunite/api/web/controller/QueueController.java
@@ -26,7 +26,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -37,7 +36,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"${client.web.url}"})
 @RequiredArgsConstructor
 @RequestMapping(path = "/queues", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Queue Controller")
